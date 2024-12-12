01627a01ef2d298310e313f6c216f16be53a1b83
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-12-12 17:01:24 +0100

    Added Array with object to generate cards

--- src/components/MainComponents.jsx
+++ src/components/MainComponents.jsx
@@ -1,22 +1,34 @@
+import posts from "../data/post"
+
 
 function Main() {
-    return (
 
-        <main>
-            <div className=" d-flex justify-content-center dimension">
-                <div className="containerCard border rounded-4 flex-shrink-0 bg-white">
-                    <img src="img/league_of_legends_wild_rift_cover.jpeg" className="card-img-top rounded-top-4" alt="..." />
-                    <div className="card-body p-3">
-                        <h5 className="card-title">Titolo del post</h5>
-                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eius molestiae, esse rem excepturi unde sed, voluptates veritatis velit doloribus tempore explicabo fugit qui, vel iste quia consectetur ipsa earum?</p>
-                        <a href="#" className="btn btn-warning text-white">Leggi di piu</a>
-                    </div>
+    const newArray = [...posts]
+    const card = newArray.map((element) => {
+        return (
+            <div key={element.length} className="containerCard border rounded-4  bg-white mx-3">
+                <img src={element.src} className="card-img-top rounded-top-4" alt="..." />
+                <div className="card-body p-3">
+                    <h5 className="card-title">{element.titolo}</h5>
+                    <p className="card-text">{element.testo}</p>
+                    <a href="#" className="btn btn-warning text-white">Leggi di piu</a>
                 </div>
             </div>
-        </main>
+        )
+    })
+
+
+    return (
+
+        < main >
+            <div className=" d-flex justify-content-center dimension">
+                {card}
+            </div>
+        </main >
+
 
     )
 
 }
 
-export default Main
+export default Main;
---
+++ src/data/post.js
@@ -0,0 +1,30 @@
+const posts = [
+    {
+        src: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg",
+        titolo: "La rinascita di Aatrox",
+        testo: "Scopri la storia dietro l'Oscuro Mietitore.",
+    },
+    {
+        src: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_0.jpg",
+        titolo: "Ahri: la volpe a nove code",
+        testo: "Un viaggio tra fascino e potere arcano.",
+    },
+    {
+        src: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ashe_0.jpg",
+        titolo: "Ashe, la regina del ghiaccio",
+        testo: "Domina la Landa con la precisione glaciale.",
+    },
+    {
+        src: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ekko_0.jpg",
+        titolo: "Ekko e il tempo in frantumi",
+        testo: "Rivoluziona le battaglie con Ekko.",
+    },
+    {
+        src: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jinx_0.jpg",
+        titolo: "Jinx: caos e risate",
+        testo: "Porta il caos in ogni partita con Jinx.",
+    },
+];
+
+
+export default posts;
