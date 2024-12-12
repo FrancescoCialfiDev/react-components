de59b8df12581d454521f9da929f2dfbda0ed94d
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-12-12 16:21:26 +0100

    added style

--- src/components/MainComponents.jsx
+++ src/components/MainComponents.jsx
@@ -4,12 +4,12 @@ function Main() {
 
         <main>
             <div className=" d-flex justify-content-center dimension">
-                <div className="containerCard border rounded-3 flex-shrink-0">
-                    <img src="img/league_of_legends_wild_rift_cover.jpeg" className="card-img-top rounded-top" alt="..." />
+                <div className="containerCard border rounded-4 flex-shrink-0 bg-white">
+                    <img src="img/league_of_legends_wild_rift_cover.jpeg" className="card-img-top rounded-top-4" alt="..." />
                     <div className="card-body p-3">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <h5 className="card-title">Titolo del post</h5>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eius molestiae, esse rem excepturi unde sed, voluptates veritatis velit doloribus tempore explicabo fugit qui, vel iste quia consectetur ipsa earum?</p>
+                        <a href="#" className="btn btn-warning text-white">Leggi di piu</a>
                     </div>
                 </div>
             </div>
